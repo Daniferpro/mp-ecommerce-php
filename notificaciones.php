54894c6db470a82a0829d6ada67fed7140ddfe99
Daniel Ferreira <daniferpro3@gmail.com>
2021-06-23 01:04:57 -0300

    update 6

--- notificaciones.php
+++ notificaciones.php
@@ -1,1 +1,59 @@
 <?php
+
+require __DIR__ .  '/vendor/autoload.php';
+
+
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+
+
+$datos = [
+    'type' => $_REQUEST['type'],
+    'topic' => $_REQUEST['topic'],
+    'id' => $_REQUEST['id'],
+];
+
+if($_REQUEST['topic']=="payment" || $_REQUEST['topic'=="merchant_order"]){
+    file_put_contents('webhook.log', json_encode($datos) . PHP_EOL,FILE_APPEND);
+}
+
+switch ($type) {
+    case "payment":
+        $payment = MercadoPago\Payment::find_by_id($id);
+        if (!empty($payment)) {
+            header("HTTP/1.1 200 OK");
+
+        } else {
+            header("HTTP/1.1 400 NOT_OK");
+        }
+        break;
+    case "plan":
+        $plan = MercadoPago\Plan::find_by_id($id);
+        if (!empty($plan)) {
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 400 NOT_OK");
+        }
+        break;
+    case "subscription":
+        $plan = MercadoPago\Subscription::find_by_id($id);
+        if (!empty($plan)) {
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 400 NOT_OK");
+        }
+        break;
+    case "invoice":
+        $plan = MercadoPago\Invoice::find_by_id($id);
+        if (!empty($plan)) {
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 400 NOT_OK");
+        }
+        break;
+    default:
+        header("HTTP/1.1 200 OK");
+        break;
+
+}
+
+?>
